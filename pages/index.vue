e26b79d3b1c8538409cccdce6b1ba6b8b888a255
parsehex <1779937+parsehex@users.noreply.github.com>
2025-01-01 05:58:00 -0500

    Update index.vue

--- pages/index.vue
+++ pages/index.vue
@@ -7,14 +7,8 @@ definePageMeta({
   keywords: "Home",
 });
 </script>
-
 <template>
   <div class="relative h-full flex flex-col items-center gap-4 mx-auto max-w-7xl">
-    <!-- grid -->
-    <div
-      class="pointer-events-none absolute inset-0 bg-center bg-grid-black/10 dark:bg-grid-white/5 bg-grid-14 [mask-image:radial-gradient(white,transparent_80%)]"
-    ></div>
-
     <RoomManager />
   </div>
 </template>
