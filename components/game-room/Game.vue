52b333c46e28c5ceb77f1c21b34892ebcc225696
parsehex <1779937+parsehex@users.noreply.github.com>
2025-01-10 10:35:20 -0500

    fix game component

--- components/game-room/Game.vue
+++ components/game-room/Game.vue
@@ -53,11 +53,12 @@ watch(() => sock.thisRoom.value?.lastAiResponse?.tts, (newTTS) => {
     <div class="flex-1 overflow-y-auto p-4 space-y-4">
       <div v-if="room?.history" class="prose dark:prose-invert max-w-none">
         <div v-for="(msg, i) in room?.history" :key="i">
-          <span v-if="msg.type === 'intro' || msg.type === 'narrative'">{{ msg.text }}</span>
-          <span v-else-if="msg.type === 'choice'">
+          <p>{{ msg.intro }}</p>
+          <p>{{ msg.narrative }}</p>
+          <p>
             <span class="text-muted">{{ msg.player }} chose: </span>
-            <span class="font-bold">{{ msg.text }}</span>
-          </span>
+            <span class="font-bold">{{ msg.choice }}</span>
+          </p>
         </div>
       </div>
       <div v-if="isAiLoading" class="text-center flex flex-col items-center space-y-2">
