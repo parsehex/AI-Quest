e7eabb491e0c530b946784aa64195158bfcaeacf
parsehex <1779937+parsehex@users.noreply.github.com>
2025-01-18 19:12:47 -0500

    add logo, GH link to navbar

--- components/layout/Navbar.vue
+++ components/layout/Navbar.vue
@@ -11,13 +11,20 @@ const gameStatus = useGameStatus();
           class="z-10 h-[50px] sm:h-[45px] flex items-center justify-around gap-2 sm:hover:gap-4 p-1 transition-all duration-300 ease-in-out">
           <Connection />
           <span v-if="!gameStatus.isActive.value" class="text-red-500">Game inactive</span>
+          <NuxtLink aria-label="logo-home navigation link" :class="['text-black/60 dark:text-muted',
+          ]" :to="'/'"
+            class="flex items-center rounded-full px-4 sm:px-6 py-1 border border-transparent hover:text-black hover:bg-white-900/50 hover:border-white/5 dark:hover:text-white/70 duration-300 ease-in-out transition-all">
+            <!-- <component :is="item.icon" /> -->
+            <NuxtImg src="/logo-64px.png" class="w-8 h-8" />
+          </NuxtLink>
           <NuxtLink v-for="item in navigation" :id="item.name.toLowerCase()" :key="item.name"
             :aria-label="item.name + ' navigation link'" :class="[
               item.name === $route.name
                 ? 'text-black/80 dark:text-white/75 shadow-black/50 dark:shadow-white/50 shadow-2xl border border-white/5 backdrop-blur-3xl bg-zinc-900/10'
                 : 'text-black/60 dark:text-muted',
             ]" :to="item.to"
-            class="flex items-center rounded-full px-4 sm:px-6 py-1 border border-transparent hover:text-black hover:bg-white-900/50 hover:border-white/5 dark:hover:text-white/70 duration-300 ease-in-out transition-all">
+            class="flex items-center rounded-full px-4 sm:px-6 py-1 border border-transparent hover:text-black hover:bg-white-900/50 hover:border-white/5 dark:hover:text-white/70 duration-300 ease-in-out transition-all"
+            :target="item.target">
             <component :is="item.icon" class="w-7 h-7 sm:w-6 sm:h-6 font-medium" />
             <span class="hidden sm:inline-block ml-2 font-medium">{{ item.name }}</span>
           </NuxtLink>
--- composables/useNavigation.ts
+++ composables/useNavigation.ts
@@ -1,6 +1,7 @@
 import {
   HomeIcon, PresentationChartBarIcon
 } from "@heroicons/vue/24/outline";
+import { IconBrandGithub } from '@tabler/icons-vue';
 import type { FunctionalComponent } from "@vue/runtime-core";
 
 type Where = "home" | "app" | "user" | "admin";
@@ -9,18 +10,20 @@ type Navigation = {
   name: string;
   to: string;
   icon: FunctionalComponent;
+  target?: string;
 };
 
 export function getNavigation(where: Where): Navigation[] {
   const isDev = import.meta.env.DEV;
   switch (where) {
     case "home":
-      const routes = [
+      const routes: Navigation[] = [
         { name: "Home", to: "/", icon: HomeIcon },
       ];
       if (isDev) {
         routes.push({ name: "Admin", to: "/admin", icon: PresentationChartBarIcon });
       }
+      routes.push({ name: "", to: "https://github.com/parsehex/AI-Quest", icon: IconBrandGithub, target: '_blank' });
       return routes;
     default:
       return [];
